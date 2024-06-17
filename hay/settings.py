e4800748239fe8355882bec2813e8f2d4473dd04
MarioLFB <popesco19@hotmail.com>
2024-06-17 20:06:18 +0100

    Add users app. remove unused template

--- hay/settings.py
+++ hay/settings.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 import dj_database_url
+
+
 if os.path.isfile('env.py'):
     import env
 
@@ -31,6 +33,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'django.contrib.staticfiles',
+    'users',
 ]
 
 SITE_ID = 1
@@ -124,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
