f876e47da15167a95ab6ab04b94e9c2dadeb61fe
MarioLFB <popesco19@hotmail.com>
2024-06-19 18:53:57 +0100

    Update URLs for home, menu, bookings, and contact views

--- hay/urls.py
+++ hay/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('', include('menu.urls')),
-    path('', include('bookings.urls')),
-    path('', include('contact.urls')),
+    path('home/', include('home.urls')),
+    path('menu/', include('menu.urls')),
+    path('bookings/', include('bookings.urls')),
+    path('contact/', include('contact.urls')),
     path('accounts/', include('users.urls')),
     ]
