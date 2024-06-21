469958695eac53d04ffcc146e8013679946139e0
MarioLFB <popesco19@hotmail.com>
2024-06-21 17:23:50 +0100

    Add ContactForm model for handling contact form submissions

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class ContactForm(models.Model):
+    name = models.CharField(max_length=100, label='Name')
+    email = models.EmailField(label='Email')
+    message = models.TextField(widget=forms.Textarea, label='Message')
+
+    def __str__(self):
+        return self.name
