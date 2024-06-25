07810e9933a9e3b24895f285e1c3ef3711a8110b
MarioLFB <popesco19@hotmail.com>
2024-06-25 20:02:04 +0100

    Update TableForm and Table model to include 'table_data', 'table_phone', 'table_email', 'table_date', and 'table_time' fields

--- bookings/forms.py
+++ bookings/forms.py
@@ -2,9 +2,21 @@ from .models import Table
 from django import forms
 
 class TableForm(forms.ModelForm):
+    table_capacity = forms.ChoiceField(choices=[(i, i) for i in range(1, 11)], label='Number of people')
+
     class Meta:
         model = Table
-        fields = ['table_name', 'table_capacity', 'table_data']
+        fields = ['table_name', 'table_phone', 'table_email', 'table_capacity', 'table_date', 'table_time']
         widgets = {
-            'table_data': forms.DateInput(attrs={'class': 'form-control', 'id': 'datepicker'}),
+            'table_date': forms.DateInput(attrs={'type': 'date'}),
+            'table_time': forms.TimeInput(attrs={'type': 'time'}),
         }
+
+        labels = {
+            'table_name': 'Name',
+            'table_phone': 'Phone',
+            'table_email': 'Email',
+            'table_date': 'Date',
+            'table_time': 'Time',
+        }
+
--- bookings/models.py
+++ bookings/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from datetime import date
 
 # Create your models here.
 
 class Table(models.Model):
     table_name = models.CharField(max_length=100)
+    table_phone = models.CharField(max_length=100, help_text='Enter your phone number', default='+44 20 7123 4567')
+    table_email = models.EmailField(default='email@email.com')
     table_capacity = models.IntegerField(
-        validators=[MinValueValidator(1), MaxValueValidator(10)]
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
     )
-    table_data = models.DateField()
+    table_date = models.DateField(default=date.today)
+    table_time = models.TimeField(default='08:00:00')
 
     def __str__(self):
         return self.table_name
