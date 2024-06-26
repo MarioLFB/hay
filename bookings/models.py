968b38aa7c26869fe85c6fa8bca9cad47eb8744b
MarioLFB <popesco19@hotmail.com>
2024-06-26 18:00:18 +0100

    Update models and views for user bookings functionality

--- bookings/models.py
+++ bookings/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 from datetime import date
 
 # Create your models here.
@@ -13,6 +14,7 @@ class Table(models.Model):
     )
     table_date = models.DateField(default=date.today)
     table_time = models.TimeField(default='08:00:00')
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.table_name
--- bookings/views.py
+++ bookings/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, redirect
 from .forms import TableForm
 from .models import Table
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def booking_table(request):
     if request.method == 'POST':
         form = TableForm(request.POST)
         if form.is_valid():
-            form.save()
+            table = form.save(commit=False)
+            table.user = request.user
+            table.save()
             return redirect('booked')
     else:
         form = TableForm()
@@ -16,6 +21,8 @@ def booking_table(request):
 def booked_view(request):
     return render(request, 'bookings/booked.html')
 
+
+@login_required
 def my_bookings(request):
-    bookings = Table.objects.all()
+    bookings = Table.objects.filter(user=request.user)
     return render(request, 'bookings/mybookings.html', {'bookings': bookings})
