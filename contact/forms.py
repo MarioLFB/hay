51fad59d5dc65807309335f4adc38125299dc864
MarioLFB <popesco19@hotmail.com>
2024-06-28 18:15:48 +0100

    Improve contact form UI and functionality

--- contact/forms.py
+++ contact/forms.py
@@ -6,3 +6,8 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactMessage
         fields = ['name', 'email', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(attrs={'class': 'form-control'}),
+        }
