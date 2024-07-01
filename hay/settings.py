d5b1eb1a143ada8cff5e0fd2231e367c27f77bd6
MarioLFB <popesco19@hotmail.com>
2024-07-01 17:22:31 +0100

    Update DATABASES configuration in settings.py

--- hay/settings.py
+++ hay/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'hay.wsgi.application'
 #}
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
