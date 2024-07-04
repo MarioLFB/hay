f2c4c6642fdc75d9dcb12a5d77dd83d8b9cdd2ea
MarioLFB <popesco19@hotmail.com>
2024-07-04 18:43:21 +0100

    Refactor views.py to remove unused import and optimize imports

--- bookings/views.py
+++ bookings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from .forms import TableForm
 from .models import Table
 from django.contrib.auth.decorators import login_required
