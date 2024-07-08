1c3be5622f594a90f9f0a37578981093a365d43b
MarioLFB <popesco19@hotmail.com>
2024-07-08 18:50:27 +0100

    Add email validation to ContactMessage model

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.core.validators import EmailValidator
+from validate_email import validate_email
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
@@ -8,6 +10,13 @@ class ContactMessage(models.Model):
     email = models.EmailField()
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def clean(self):
+        super().clean()
+        if not validate_email(self.email):
+            raise ValidationError({'email': 'Invalid email format'})
+
+
 
     def __str__(self):
         return self.name
