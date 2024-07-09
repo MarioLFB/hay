631690569b9ba7edc7ff653c29e42c070f819801
MarioLFB <popesco19@hotmail.com>
2024-07-09 21:05:52 +0100

    Remove unused CSS files and JavaScript code for login success message fade-out

--- staticfiles/js/script.54cc89378c0e.js
+++
@@ -1,9 +0,0 @@
-
-// Fade out the success message for login after 2 seconds
-$(document).ready(function() {
-    setTimeout(function() {
-        $('#success-message').fadeOut('slow');
-    }, 2000); // 2000 milissegundos = 2 seconds
-});
-
-
