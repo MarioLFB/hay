f28092b6e4ff2cbd05e2d9eba828323cf065ee6e
MarioLFB <popesco19@hotmail.com>
2024-07-10 17:57:07 +0100

    Update menu navigation link to point to the correct URL

--- menu/urls.py
+++ menu/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.menu, name='menu'),
-    path('food/', views.menu_food, name='food'),
     path('drinks/', views.menu_drinks, name='drinks'),
 ]
