3faf460a0bf13db6dc497d75bd1ab9254ec3d5b1
MarioLFB <popesco19@hotmail.com>
2024-07-26 17:55:05 +0100

    Refactor ContactForm in contact/forms.py for improved readability and maintainability

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import ContactMessage
 
+
 class ContactForm(forms.ModelForm):
     '''
     Class to create a form for the ContactMessage model
@@ -9,7 +10,13 @@ class ContactForm(forms.ModelForm):
         model = ContactMessage
         fields = ['name', 'email', 'message']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control custom-border-radius'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control custom-border-radius'}),
-            'message': forms.Textarea(attrs={'class': 'form-control custom-border-radius'}),
+            'name': forms.TextInput(
+                attrs={'class': 'form-control custom-border-radius'}
+            ),
+            'email': forms.EmailInput(
+                attrs={'class': 'form-control custom-border-radius'}
+            ),
+            'message': forms.Textarea(
+                attrs={'class': 'form-control custom-border-radius'}
+            ),
         }
