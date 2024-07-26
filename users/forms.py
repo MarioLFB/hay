9f05c488bdb967b4f4440a8b4d26c823bd36ec6d
MarioLFB <popesco19@hotmail.com>
2024-07-26 18:28:24 +0100

    Clean up code formatting and remove unnecessary whitespace in users/forms.py

--- users/forms.py
+++ users/forms.py
@@ -3,22 +3,74 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
+
 class UserLoginForm(forms.Form):
     '''
     Form to log in a user to the website using their username and password
     '''
-    username = forms.CharField(label='Username', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control custom-border-radius', 'placeholder': 'Username'}))
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control custom-border-radius', 'placeholder': 'Password'}))
+    username = forms.CharField(
+        label='Username',
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control custom-border-radius',
+                'placeholder': 'Username'
+            }
+        )
+    )
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control custom-border-radius',
+                'placeholder': 'Password'
+            }
+        )
+    )
+
 
 class UserRegisterForm(forms.Form):
     '''
-    Form to register a user to the website using their username, email and password
+    Form to register a user to the website using their username,
+    email and password
     '''
-    username = forms.CharField(label='Username', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control custom-border-radius', 'placeholder': 'Username'}))
-    email = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'class': 'form-control custom-border-radius', 'placeholder': 'Email'}))
-    password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control custom-border-radius', 'placeholder': 'Password'}))
-    password_confirm = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control custom-border-radius', 'placeholder': 'Confirm Password'}))
-
+    username = forms.CharField(
+        label='Username',
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control custom-border-radius',
+                'placeholder': 'Username'
+            }
+        )
+    )
+    email = forms.EmailField(
+        label='Email',
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control custom-border-radius',
+                'placeholder': 'Email'
+            }
+        )
+    )
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control custom-border-radius',
+                'placeholder': 'Password'
+            }
+        )
+    )
+    password_confirm = forms.CharField(
+        label='Confirm Password',
+        widget=forms.PasswordInput(
+            attrs={
+                'class': 'form-control custom-border-radius',
+                'placeholder': 'Confirm Password'
+            }
+        )
+    )
 
     def clean_username(self):
         username = self.cleaned_data['username']
@@ -26,22 +78,42 @@ class UserRegisterForm(forms.Form):
         max_length = 20
 
         if len(username) < min_length:
-            raise forms.ValidationError(_('Username must be at least {} characters long.').format(min_length))
+            raise forms.ValidationError(
+                _(
+                    'Username must be at least {} characters long.'
+                ).format(min_length)
+            )
         if len(username) > max_length:
-            raise forms.ValidationError(_('Username must be at most {} characters long.').format(max_length))
+            raise forms.ValidationError(
+                _(
+                    'Username must be at most {} characters long.'
+                ).format(max_length)
+            )
 
         return username
 
     def clean_password(self):
         password = self.cleaned_data['password']
         min_length = 8
-        
+
         if len(password) < min_length:
-            raise forms.ValidationError(_('Password must be at least {} characters long.').format(min_length))
+            raise forms.ValidationError(
+                _(
+                    'Password must be at least {} characters long.'
+                ).format(min_length)
+            )
         if not any(char.isdigit() for char in password):
-            raise forms.ValidationError(_('Password must contain at least one digit.'))
+            raise forms.ValidationError(
+                _(
+                    'Password must contain at least one digit.'
+                )
+            )
         if not any(char.isalpha() for char in password):
-            raise forms.ValidationError(_('Password must contain at least one letter.'))
+            raise forms.ValidationError(
+                _(
+                    'Password must contain at least one letter.'
+                )
+            )
 
         return password
 
