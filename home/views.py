5fd7db78dc348235ace9f123ff29f7092e2def0f
MarioLFB <popesco19@hotmail.com>
2024-07-26 18:21:27 +0100

    Clean up code formatting and remove unnecessary whitespace in views.py files

--- home/views.py
+++ home/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
     '''
     Function to display the home page
     '''
     return render(request, 'home/home.html')
+
 
 def handler404(request, exception):
     '''
--- menu/views.py
+++ menu/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def menu(request):
     '''
     Function to display the menu page
     '''
     return render(request, 'menu/menu.html')
+
 
 def menu_drinks(request):
     '''
