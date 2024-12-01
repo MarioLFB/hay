d2aa71146abb960c7c869a991122b016f7fc63a2
Mario Borges <popesco19@hotmail.com>
2024-12-01 13:12:25 +0000

    Restrict booking times to 30-minute intervals between 11:30 am and 10:00 pm

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,7 +1,18 @@
 from django import forms
 from .models import Table
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 
+
+TIME_CHOICES = []
+start_time = datetime.strptime('11:30', '%H:%M')
+end_time = datetime.strptime('22:00', '%H:%M')
+
+current_time = start_time
+while current_time <= end_time:
+    time_str = current_time.strftime('%H:%M')
+    display_time = current_time.strftime('%I:%M %p')
+    TIME_CHOICES.append((time_str, display_time))
+    current_time += timedelta(minutes=30)
 
 
 class TableForm(forms.ModelForm):
@@ -11,6 +22,14 @@ class TableForm(forms.ModelForm):
     table_capacity = forms.ChoiceField(
         choices=[(i, i) for i in range(1, 11)],
         label='Number of people'
+    )
+
+    table_time = forms.ChoiceField(
+        choices=TIME_CHOICES,
+        widget=forms.Select(
+            attrs={'class': 'form-control custom-border-radius'}
+        ),
+        label='Time'
     )
 
     class Meta:
@@ -39,25 +58,17 @@ class TableForm(forms.ModelForm):
                     'min': date.today().strftime('%Y-%m-%d')
                 }
             ),
-            'table_time': forms.TimeInput(
-                attrs={
-                    'type': 'time',
-                    'class': 'form-control custom-border-radius'
-                }
-            ),
         }
         labels = {
             'table_name': 'Name',
             'table_phone': 'Phone',
             'table_email': 'Email',
             'table_date': 'Date',
-            'table_time': 'Time',
         }
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
         super().__init__(*args, **kwargs)
-        
 
     def clean(self):
         cleaned_data = super().clean()
@@ -69,9 +80,15 @@ class TableForm(forms.ModelForm):
         table_date = cleaned_data.get('table_date')
         table_time = cleaned_data.get('table_time')
 
+        if table_time:
+            table_time_obj = datetime.strptime(table_time, '%H:%M').time()
+            cleaned_data['table_time'] = table_time_obj
+
         if table_date == date.today() and table_time:
             now = datetime.now().time()
-            if table_time < now:
-                raise forms.ValidationError("You cannot book a time in the past.")
+            if table_time_obj < now:
+                raise forms.ValidationError(
+                    "You cannot book a time in the past."
+                )
 
         return cleaned_data
